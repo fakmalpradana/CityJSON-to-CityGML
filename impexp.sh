d9559878f2e57751bf88527cfa49023c97af49c6
Fairuz Akmal Pradana <fakmalpradana@gmail.com>
2025-06-09 22:53:42 +0800

    add shell script to import and export

---
+++ impexp.sh
@@ -0,0 +1,10 @@
+/Applications/3DCityDB-Importer-Exporter/bin/impexp \
+    -T postgresql \
+    -H localhost \
+    -P 5432 \
+    -d dkj \
+    -S citydb \
+    -u postgres \
+    -p admin1234 \
+    50/AG_07/AG_07_D/AG_07_D.json \
+    import
