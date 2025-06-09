10cb592a3664c087cba5205931b82b6bd78fbf10
Fairuz Akmal Pradana <fakmalpradana@gmail.com>
2025-06-10 00:12:03 +0800

    add import export single file

--- impexp.sh
+++ impexp.sh
@@ -1,4 +1,5 @@
 /Applications/3DCityDB-Importer-Exporter/bin/impexp \
+    import \
     -T postgresql \
     -H localhost \
     -P 5432 \
@@ -6,5 +7,20 @@
     -S citydb \
     -u postgres \
     -p admin1234 \
-    50/AG_07/AG_07_D/AG_07_D.json \
-    import
+    50/AH_03/AH_03_B/AH_03_B.json \
+
+/Applications/3DCityDB-Importer-Exporter/bin/impexp \
+    export \
+    -T postgresql \
+    -H localhost \
+    -P 5432 \
+    -d dkj \
+    -S citydb \
+    -u postgres \
+    -p admin1234 \
+    -o 50/CityGML/AH_30_B.gml \
+    --compressed-format citygml \
+    --replace-ids \
+    --id-prefix AH_30_B_
+
+sh resetdb.sh
